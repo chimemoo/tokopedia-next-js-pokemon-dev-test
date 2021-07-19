95a689e1a8e5b60391d7ecb60a610dd00d435bec
chimemoo <christ.16062@student.unsika.ac.id>
2021-07-19 13:07:39 +0700

    add service worker

--- pages/_app.js
+++ pages/_app.js
@@ -3,8 +3,16 @@ import { GlobalStyles } from '../commons/styles/global';
 import ContextProvider from '../commons/context';
 import Layout from '../components/commons/Layout';
 import client from '../commons/graphql/client';
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js').catch(err => console.error('Service worker registration failed', err));
+    } else {
+      console.log('Service worker not supported');
+    }
+  }, []);
   return (
     <ApolloProvider client={client}>
       <ContextProvider>
